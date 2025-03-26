fb1798ec13fb8aa46a6f14eba8346da002da1594
Rodrigo Widmann <rodrigo_widmann@hotmail.com>
2025-03-26 20:52:14 -0300

    config prisma

--- server/prisma/migrations/20241125214312_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "servicos" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "nome" TEXT NOT NULL,
-    "descricao" TEXT NOT NULL,
-    "descricaoProposta" TEXT NOT NULL,
-    "valor" REAL NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
